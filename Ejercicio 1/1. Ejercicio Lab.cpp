e2416c104acd199ec452496dcc444b19cf95f7dc
Miguel Velasquez <miguelvelasquezvb.ma@gmail.com>
2024-03-30 14:07:17 -0300

    Agregando mi primer ejercicio

---
+++ Ejercicio 1/1. Ejercicio Lab.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int workH, sueldo, valorS;
+    cout<<"Por favor ingrese la cantidad de horas trabajadas: ";
+    cin>>workH;
+    cout<< "ingrese valor de hora: ";
+    cin>>valorS;
+    sueldo= workH*valorS;
+    cout<<"Su sueldo es: "<<sueldo;
+
+
+
+
+ return 0;
+}
